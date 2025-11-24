7934fab33fa6044bce629fbf00bcb66e3d83c628
ArundhotiKar <arundhotikar60@gmail.com>
2025-11-24 23:03:39 +0600

    .env.local added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCULLwW-fag0hpwmdgJwOhtnWdRvw6DvF8",
-  authDomain: "toytopia-6ded1.firebaseapp.com",
-  projectId: "toytopia-6ded1",
-  storageBucket: "toytopia-6ded1.firebasestorage.app",
-  messagingSenderId: "37010133246",
-  appId: "1:37010133246:web:debe14ec7d70affc1a15af"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
